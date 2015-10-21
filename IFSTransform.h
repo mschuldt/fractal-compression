fcc4a6b5108fece9aebe11114a3e2b2c4dc42ea1
mschuldt <mbschuldt@gmail.com>
2015-10-21 15:32:38 -0700

    cleanup indentation and whitespace

--- IFSTransform.h
+++ IFSTransform.h
@@ -1,18 +1,18 @@
 /*
  * Fractal Image Compression. Copyright 2004 Alex Kennberg.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
-*/
+ */
 
 #ifndef IFST_H
 #define IFST_H
@@ -21,72 +21,72 @@ typedef vector<class IFSTransform*> Transform;
 
 class Transforms
 {
-public:
-	Transforms();
+ public:
+  Transforms();
 
-	~Transforms();
+  ~Transforms();
 
-public:
-	Transform ch[3];
-	int channels;
+ public:
+  Transform ch[3];
+  int channels;
 };
 
 
 class IFSTransform
 {
-public:
+ public:
 
-	enum SYM
-	{
-		SYM_NONE = 0,
-		SYM_R90,
-		SYM_R180,
-		SYM_R270,
-		SYM_HFLIP,
-		SYM_VFLIP,
-		SYM_FDFLIP,
-		SYM_RDFLIP,
-		SYM_MAX
-	};
+  enum SYM
+  {
+    SYM_NONE = 0,
+    SYM_R90,
+    SYM_R180,
+    SYM_R270,
+    SYM_HFLIP,
+    SYM_VFLIP,
+    SYM_FDFLIP,
+    SYM_RDFLIP,
+    SYM_MAX
+  };
 
-public:
+ public:
 
-	static PixelValue* DownSample(PixelValue* src, int srcWidth,
-		int startX, int startY, int targetSize);
+  static PixelValue* DownSample(PixelValue* src, int srcWidth,
+                                int startX, int startY, int targetSize);
 
-public:
+ public:
 
-	IFSTransform(int fromX, int fromY, int toX, int toY, int size,
-		SYM symmetry, double scale, int offset);
+  IFSTransform(int fromX, int fromY, int toX, int toY, int size,
+               SYM symmetry, double scale, int offset);
 
-	~IFSTransform();
+  ~IFSTransform();
 
-	void Execute(PixelValue* src, int srcWidth,
-		PixelValue* dest, int destWidth, bool downsampled);
+  void Execute(PixelValue* src, int srcWidth,
+               PixelValue* dest, int destWidth, bool downsampled);
 
-private:
+ private:
 
-	bool isScanlineOrder();
+  bool isScanlineOrder();
 
-	bool isPositiveX();
+  bool isPositiveX();
 
-	bool isPositiveY();
+  bool isPositiveY();
 
-private:
+ private:
 
-	// Spatial transformation
-	int fromX;
-	int fromY;
-	int toX;
-	int toY;
-	int size;
+  // Spatial transformation
+  int fromX;
+  int fromY;
+  int toX;
+  int toY;
+  int size;
 
-	// Symmetry operation
-	SYM symmetry;
+  // Symmetry operation
+  SYM symmetry;
 
-	// Pixel intensity
-	double scale;
-	int offset;
+  // Pixel intensity
+  double scale;
+  int offset;
 
 };
 
--- Image.h
+++ Image.h
@@ -1,18 +1,18 @@
 /*
  * Fractal Image Compression. Copyright 2004 Alex Kennberg.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
-*/
+ */
 
 #ifndef IMAGE_H
 #define IMAGE_H
@@ -21,58 +21,58 @@ typedef unsigned char PixelValue;
 
 class ImageData
 {
-public:
-	ImageData();
+ public:
+  ImageData();
 
-	~ImageData();
+  ~ImageData();
 
-public:
-	int width;
-	int height;
-	int channels;
-	PixelValue* imagedata;
-	PixelValue* imagedata2;
-	PixelValue* imagedata3;
+ public:
+  int width;
+  int height;
+  int channels;
+  PixelValue* imagedata;
+  PixelValue* imagedata2;
+  PixelValue* imagedata3;
 };
 
 
 class Image
 {
-public:
+ public:
 
-	Image(string fileName);
+  Image(string fileName);
 
-	~Image();
+  ~Image();
 
-	void Load();
+  void Load();
 
-	void Save();
+  void Save();
 
-	void GetChannelData(int channel, PixelValue *buffer, int size);
+  void GetChannelData(int channel, PixelValue *buffer, int size);
 
-	void SetChannelData(int channel, PixelValue *buffer, int size);
+  void SetChannelData(int channel, PixelValue *buffer, int size);
 
-	int GetWidth();
+  int GetWidth();
 
-	int GetHeight();
+  int GetHeight();
 
-	int GetChannels();
+  int GetChannels();
 
-	int GetOriginalSize();
+  int GetOriginalSize();
 
-private:
+ private:
 
-	void ConvertToYCbCr(PixelValue& Y, PixelValue& Cb, PixelValue& Cr,
-		PixelValue R, PixelValue G, PixelValue B);
+  void ConvertToYCbCr(PixelValue& Y, PixelValue& Cb, PixelValue& Cr,
+                      PixelValue R, PixelValue G, PixelValue B);
 
-	void ConvertFromYCbCr(PixelValue& R, PixelValue& G, PixelValue& B,
-		PixelValue Y, PixelValue Cb, PixelValue Cr);
+  void ConvertFromYCbCr(PixelValue& R, PixelValue& G, PixelValue& B,
+                        PixelValue Y, PixelValue Cb, PixelValue Cr);
 
-private:
+ private:
 
-	string fileName;
-	ImageData img;
-	int originalSize;
+  string fileName;
+  ImageData img;
+  int originalSize;
 };
 
 #endif // IMAGE_H
