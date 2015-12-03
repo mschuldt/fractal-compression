8e974bef26eced4d120a33a1bbd119a441a758ec
mschuldt <mbschuldt@gmail.com>
2015-12-02 22:14:36 -0800

    loop unrolling for case when size == 2

--- Encoder.cpp
+++ Encoder.cpp
@@ -91,85 +91,89 @@ double Encoder::GetError(
                          PixelValue* domainData, int domainWidth, int domainX, int domainY, int domainAvg,
                          PixelValue* rangeData, int rangeWidth, int rangeX, int rangeY, int rangeAvg,
                          int size, double scale)
-{
-  // printf("domainAvg = %d\nrangeAvg = %d\nscale = %f\n", domainAvg, rangeAvg, scale);
+  {
+    // printf("domainAvg = %d\nrangeAvg = %d\nscale = %f\n", domainAvg, rangeAvg, scale);
+    float bottom = (float)(size * size);
+    PixelValue * domain_ptr;
+    PixelValue * range_ptr;
 
-  __m128i top = _mm_setzero_si128();
-
-  float bottom = (float)(size * size);
-
-  unsigned int* temp_i = temp_ints[omp_get_thread_num()];
-  float* temp_f = temp_floats[omp_get_thread_num()];
-
-  temp_i[0] = domainAvg;
-  temp_i[1] = domainAvg;
-  temp_i[2] = domainAvg;
-  temp_i[3] = domainAvg;
-  __m128i domainAvgVec = _mm_load_si128((__m128i const *)temp_i);
-
-
-  temp_i[0] = rangeAvg;
-  temp_i[1] = rangeAvg;
-  temp_i[2] = rangeAvg;
-  temp_i[3] = rangeAvg;
-  __m128i rangeAvgVec = _mm_load_si128((__m128i const *)temp_i);
-
-  temp_f[0] = scale;
-  temp_f[1] = scale;
-  temp_f[2] = scale;
-  temp_f[3] = scale;
-  __m128 scaleVec = _mm_load_ps(temp_f);
-  PixelValue * domain_ptr;
-  PixelValue * range_ptr;
-  switch (size){
-  case 16:
-  case 8:
-  case 4:
-    for (int y = 0; y < size; y++)
-      {
-        domain_ptr = domainData + (domainY + y) * domainWidth + domainX;
-        range_ptr = rangeData + (rangeY + y) * rangeWidth + rangeX;
-        for (int x = 0; x < size; x+=4)
-          {
-            __m128i domain = _mm_load_si128((__m128i const *)(domain_ptr + x));
-            domain = _mm_sub_epi32(domain, domainAvgVec);
-            __m128i range = _mm_load_si128((__m128i const *)(range_ptr + x));
-            range = _mm_sub_epi32(range, rangeAvgVec);
-            __m128 domain_ps = _mm_cvtepi32_ps(domain);
-            __m128i scaled = _mm_cvtps_epi32(_mm_mul_ps(scaleVec, domain_ps));
-            __m128i diff = _mm_sub_epi32(scaled, range);
-            top = _mm_add_epi32(top, _mm_mullo_epi32(diff, diff));
-          }
-      }
-    break;
-
-  case 2:
-    {
-      int top = 0;
-      for (int y = 0; y < size; y++)
-        {
-          for (int x = 0; x < size; x++)
-            {
-
-              int domain = (domainData[(domainY + y) * domainWidth + (domainX + x)] - domainAvg);
-              int range = (rangeData[(rangeY + y) * rangeWidth + (rangeX + x)] - rangeAvg);
-              int diff = (int)(scale * (double)domain) - range;
-
-              // According to the formula we want (DIFF*DIFF)/(SIZE*SIZE)
-              top += (diff * diff);
-
-            }
-        }
+    if (size == 2){
+      int top;
+      int domain, range, diff;
+      domain_ptr = domainData + (domainY) * domainWidth + domainX;
+      range_ptr = rangeData + (rangeY) * rangeWidth + rangeX;          
+      diff = (int)(scale * (double)(*domain_ptr - domainAvg))
+        - (*range_ptr - rangeAvg);
+      top = (diff * diff);
+      diff = (int)(scale * (double)(*(domain_ptr + 1) - domainAvg))
+        - (*range_ptr - rangeAvg);
+      top += (diff * diff);          
+      domain_ptr = domainData + (domainY) * domainWidth + domainX;
+      range_ptr = rangeData + (rangeY) * rangeWidth + rangeX;          
+      diff = (int)(scale * (double)(*domain_ptr - domainAvg))
+        - (*(range_ptr + 1) - rangeAvg);
+      top += (diff * diff);
+      diff = (int)(scale * (double)(*(domain_ptr + 1) - domainAvg))
+        - (*(range_ptr + 1) - rangeAvg);
+      top += (diff * diff);          
       return top/bottom;
-    }
-  default:
-    printf("default case\n");
-    exit(1);
-  }
 
-  _mm_store_si128((__m128i*)temp_i, top);
-  return ((temp_i[0] + temp_i[1] + temp_i[2] + temp_i[3]) / bottom);
-}
+    } else {
+
+      __m128i top = _mm_setzero_si128();
+
+      unsigned int* temp_i = temp_ints[omp_get_thread_num()];
+      float* temp_f = temp_floats[omp_get_thread_num()];
+
+      temp_i[0] = domainAvg;
+      temp_i[1] = domainAvg;
+      temp_i[2] = domainAvg;
+      temp_i[3] = domainAvg;
+      __m128i domainAvgVec = _mm_load_si128((__m128i const *)temp_i);
+
+      temp_i[0] = rangeAvg;
+      temp_i[1] = rangeAvg;
+      temp_i[2] = rangeAvg;
+      temp_i[3] = rangeAvg;
+      __m128i rangeAvgVec = _mm_load_si128((__m128i const *)temp_i);
+
+      temp_f[0] = scale;
+      temp_f[1] = scale;
+      temp_f[2] = scale;
+      temp_f[3] = scale;
+      __m128 scaleVec = _mm_load_ps(temp_f);
+
+      switch (size){
+      case 16:
+      case 8:
+      case 4:
+        for (int y = 0; y < size; y++)
+          {
+            domain_ptr = domainData + (domainY + y) * domainWidth + domainX;
+            range_ptr = rangeData + (rangeY + y) * rangeWidth + rangeX;
+            for (int x = 0; x < size; x+=4)
+              {
+                __m128i domain = _mm_load_si128((__m128i const *)(domain_ptr + x));
+                domain = _mm_sub_epi32(domain, domainAvgVec);
+                __m128i range = _mm_load_si128((__m128i const *)(range_ptr + x));
+                range = _mm_sub_epi32(range, rangeAvgVec);
+                __m128 domain_ps = _mm_cvtepi32_ps(domain);
+                __m128i scaled = _mm_cvtps_epi32(_mm_mul_ps(scaleVec, domain_ps));
+                __m128i diff = _mm_sub_epi32(scaled, range);
+                top = _mm_add_epi32(top, _mm_mullo_epi32(diff, diff));
+              }
+          }
+        break;
+
+      default:
+        printf("default case, size=%d\n", size);
+        exit(1);
+      }
+
+      _mm_store_si128((__m128i*)temp_i, top);
+      return ((temp_i[0] + temp_i[1] + temp_i[2] + temp_i[3]) / bottom);
+    }
+  }
 
 
 int Encoder::GetAveragePixel(PixelValue* domainData, int domainWidth,
